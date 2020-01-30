41b8149c71d826def2134ccf86f2e25989dc3a44
marcinkepa20 <marcinkepa20@gmail.com>
2020-01-30 13:29:28 +0100

    backend files

---
+++ Backend/src/main/java/pierwszyPakiet/UserRepository.java
@@ -0,0 +1,11 @@
+package pierwszyPakiet;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface UserRepository extends JpaRepository <User, Long> {
+
+    List<User> findByUsername(String username);
+
+}
